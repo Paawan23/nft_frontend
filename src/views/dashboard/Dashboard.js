3bbcbb0e6e0d22d5edabb31332d6dde3423d323d
paawanEnacton <psbhatt@enacton.com>
2023-03-31 19:51:41 +0530

    topOwners and ownerDistribution done

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import {
   CAvatar,
@@ -18,7 +18,7 @@ import {
   CTableHeaderCell,
   CTableRow,
 } from '@coreui/react'
-import { CChartLine, CChartScatter } from '@coreui/react-chartjs'
+import { CChartBar, CChartLine, CChartScatter } from '@coreui/react-chartjs'
 import { getStyle, hexToRgba } from '@coreui/utils'
 import CIcon from '@coreui/icons-react'
 import {
@@ -55,6 +55,8 @@ import axios from 'axios'
 
 import WidgetsBrand from '../widgets/WidgetsBrand'
 import WidgetsDropdown from '../widgets/WidgetsDropdown'
+
+import '../../scss/_custom.scss'
 
 const Dashboard = () => {
   // const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
@@ -182,6 +184,8 @@ const Dashboard = () => {
 
   const [salesData, setSalesData] = useState({})
   const [saleTime, setSaleTime] = useState(7)
+  const [topOwners, setTopOwners] = useState([])
+  const [ownerDistribution, setOwnerDistribution] = useState({})
 
   const getSalesData = () => {
     axios
@@ -226,9 +230,50 @@ const Dashboard = () => {
       })
   }
 
+  const getTopOwnersData = () => {
+    axios
+      .get(`http://localhost:5000/getTopOwners`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        setTopOwners(res.data.data)
+        console.log('res.data :>> ', res.data)
+      })
+      .catch((error) => {
+        console.log('error.message :>> ', error.message)
+      })
+  }
+  const getOwnerDistributionData = () => {
+    axios
+      .get(`http://localhost:5000/getOwners`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        setOwnerDistribution(res.data.data)
+      })
+      .catch((error) => {
+        console.log('error.message :>> ', error.message)
+      })
+  }
+
   useEffect(() => {
     getSalesData()
+    getTopOwnersData()
+    getOwnerDistributionData()
   }, [saleTime])
+
+  const tableRef = useRef()
+
+  useEffect(() => {
+    if (tableRef.current) {
+      tableRef.current.parentElement.style.height = '250px'
+    }
+    return () => {}
+  }, [tableRef])
 
   return (
     <>
@@ -242,9 +287,6 @@ const Dashboard = () => {
               </h4>
             </CCol>
             <CCol sm={7} className="d-none d-md-block">
-              {/* <CButton color="primary" className="float-end">
-                <CIcon icon={cilCloudDownload} />
-              </CButton> */}
               <CButtonGroup className="float-end me-3">
                 {[7, 30, 90].map((value) => (
                   <CButton
@@ -276,158 +318,43 @@ const Dashboard = () => {
             }}
           />
         </CCardBody>
-        {/* <CCardFooter>
-          <CRow xs={{ cols: 1 }} md={{ cols: 5 }} className="text-center">
-            {progressExample.map((item, index) => (
-              <CCol className="mb-sm-2 mb-0" key={index}>
-                <div className="text-medium-emphasis">{item.title}</div>
-                <strong>
-                  {item.value} ({item.percent}%)
-                </strong>
-                <CProgress thin className="mt-2" color={item.color} value={item.percent} />
-              </CCol>
-            ))}
-          </CRow>
-        </CCardFooter> */}
       </CCard>
-
-      {/* <WidgetsBrand withCharts />
 
       <CRow>
         <CCol xs>
           <CCard className="mb-4">
-            <CCardHeader>Traffic {' & '} Sales</CCardHeader>
-            <CCardBody>
-              <CRow>
-                <CCol xs={12} md={6} xl={6}>
-                  <CRow>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-info py-1 px-3">
-                        <div className="text-medium-emphasis small">New Clients</div>
-                        <div className="fs-5 fw-semibold">9,123</div>
-                      </div>
-                    </CCol>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-danger py-1 px-3 mb-3">
-                        <div className="text-medium-emphasis small">Recurring Clients</div>
-                        <div className="fs-5 fw-semibold">22,643</div>
-                      </div>
-                    </CCol>
-                  </CRow>
-
-                  <hr className="mt-0" />
-                  {progressGroupExample1.map((item, index) => (
-                    <div className="progress-group mb-4" key={index}>
-                      <div className="progress-group-prepend">
-                        <span className="text-medium-emphasis small">{item.title}</span>
-                      </div>
-                      <div className="progress-group-bars">
-                        <CProgress thin color="info" value={item.value1} />
-                        <CProgress thin color="danger" value={item.value2} />
-                      </div>
-                    </div>
-                  ))}
-                </CCol>
-
-                <CCol xs={12} md={6} xl={6}>
-                  <CRow>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-warning py-1 px-3 mb-3">
-                        <div className="text-medium-emphasis small">Pageviews</div>
-                        <div className="fs-5 fw-semibold">78,623</div>
-                      </div>
-                    </CCol>
-                    <CCol sm={6}>
-                      <div className="border-start border-start-4 border-start-success py-1 px-3 mb-3">
-                        <div className="text-medium-emphasis small">Organic</div>
-                        <div className="fs-5 fw-semibold">49,123</div>
-                      </div>
-                    </CCol>
-                  </CRow>
-
-                  <hr className="mt-0" />
-
-                  {progressGroupExample2.map((item, index) => (
-                    <div className="progress-group mb-4" key={index}>
-                      <div className="progress-group-header">
-                        <CIcon className="me-2" icon={item.icon} size="lg" />
-                        <span>{item.title}</span>
-                        <span className="ms-auto fw-semibold">{item.value}%</span>
-                      </div>
-                      <div className="progress-group-bars">
-                        <CProgress thin color="warning" value={item.value} />
-                      </div>
-                    </div>
-                  ))}
-
-                  <div className="mb-5"></div>
-
-                  {progressGroupExample3.map((item, index) => (
-                    <div className="progress-group" key={index}>
-                      <div className="progress-group-header">
-                        <CIcon className="me-2" icon={item.icon} size="lg" />
-                        <span>{item.title}</span>
-                        <span className="ms-auto fw-semibold">
-                          {item.value}{' '}
-                          <span className="text-medium-emphasis small">({item.percent}%)</span>
-                        </span>
-                      </div>
-                      <div className="progress-group-bars">
-                        <CProgress thin color="success" value={item.percent} />
-                      </div>
-                    </div>
-                  ))}
-                </CCol>
-              </CRow>
-
-              <br />
-
-              <CTable align="middle" className="mb-0 border" hover responsive>
+            <CCardHeader>Top Owners</CCardHeader>
+            <CCardBody style={{ height: '300px' }}>
+              <CTable
+                align="middle"
+                className="mb-0 border CTable-class"
+                hover
+                ref={tableRef}
+                responsive
+              >
                 <CTableHead color="light">
                   <CTableRow>
-                    <CTableHeaderCell className="text-center">
-                      <CIcon icon={cilPeople} />
-                    </CTableHeaderCell>
-                    <CTableHeaderCell>User</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Country</CTableHeaderCell>
-                    <CTableHeaderCell>Usage</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Payment Method</CTableHeaderCell>
-                    <CTableHeaderCell>Activity</CTableHeaderCell>
+                    <CTableHeaderCell>Name</CTableHeaderCell>
+                    <CTableHeaderCell>Wallet</CTableHeaderCell>
+                    <CTableHeaderCell>Owned</CTableHeaderCell>
+                    <CTableHeaderCell>%Owned</CTableHeaderCell>
                   </CTableRow>
                 </CTableHead>
                 <CTableBody>
-                  {tableExample.map((item, index) => (
+                  {topOwners.map((item, index) => (
                     <CTableRow v-for="item in tableItems" key={index}>
                       <CTableDataCell className="text-center">
-                        <CAvatar size="md" src={item.avatar.src} status={item.avatar.status} />
+                        <span>{index + 1}</span>
                       </CTableDataCell>
                       <CTableDataCell>
-                        <div>{item.user.name}</div>
-                        <div className="small text-medium-emphasis">
-                          <span>{item.user.new ? 'New' : 'Recurring'}</span> | Registered:{' '}
-                          {item.user.registered}
-                        </div>
+                        <div>{item.address}</div>
                       </CTableDataCell>
-                      <CTableDataCell className="text-center">
-                        <CIcon size="xl" icon={item.country.flag} title={item.country.name} />
+
+                      <CTableDataCell>
+                        <div>{item.ownership.tokenCount}</div>
                       </CTableDataCell>
                       <CTableDataCell>
-                        <div className="clearfix">
-                          <div className="float-start">
-                            <strong>{item.usage.value}%</strong>
-                          </div>
-                          <div className="float-end">
-                            <small className="text-medium-emphasis">{item.usage.period}</small>
-                          </div>
-                        </div>
-                        <CProgress thin color={item.usage.color} value={item.usage.value} />
-                      </CTableDataCell>
-                      <CTableDataCell className="text-center">
-                        <CIcon size="xl" icon={item.payment.icon} />
-                      </CTableDataCell>
-                      <CTableDataCell>
-                        <div className="small text-medium-emphasis">Last login</div>
-                        <strong>{item.activity}</strong>
+                        <div>{item.ownership.tokenCount / 100}</div>
                       </CTableDataCell>
                     </CTableRow>
                   ))}
@@ -436,7 +363,41 @@ const Dashboard = () => {
             </CCardBody>
           </CCard>
         </CCol>
-      </CRow> */}
+      </CRow>
+      <CCol xs={6}>
+        <CCard className="mb-4">
+          <CCardHeader>Owner Distribution</CCardHeader>
+          <CCardBody>
+            <CChartBar
+              data={{
+                labels: [
+                  '1 item',
+                  '2-3 items',
+                  '4-10 items',
+                  '11-25 items',
+                  '26-50 items',
+                  '51+ items',
+                ],
+                datasets: [
+                  {
+                    backgroundColor: '#f87979',
+
+                    data: [
+                      ownerDistribution.oneItem,
+                      ownerDistribution.secondThirdItem,
+                      ownerDistribution.fourTenthItem,
+                      ownerDistribution.elevenTwentyFifthItem,
+                      ownerDistribution.twentySixFiftyItem,
+                      ownerDistribution.fiftyOnePlusItem,
+                    ],
+                  },
+                ],
+              }}
+              labels="months"
+            />
+          </CCardBody>
+        </CCard>
+      </CCol>
     </>
   )
 }
